8f5711fd467ae64338df1f523f8d936c389ff3ef
Sander Jyhne <sandsjyhne@gmail.com>
2021-03-17 22:14:31 +0100

    Updated the q_agent

--- q_agent.py
+++ q_agent.py
@@ -3,9 +3,15 @@ import numpy as np
 import random
 from tqdm import tqdm
 
+import matplotlib.pyplot as plt
+
 #[0, 1, 2, 3, 4]
 # O, -, XX, -, -
 # [2, 2, 2, 2]
+
+
+EPISODES = 500
+STEPS = 20
 
 class RandomAgent:
     def __init__(self):
@@ -40,14 +46,16 @@ class RandomAgent:
 
 class QAgent:
     
-    def __init__(self, obs_space, action_space, lr=0.1, discount=0.9, epsilon=1):
+    def __init__(self, obs_space, action_space, lr=0.1, discount=0.4, epsilon=0.8):
         self.lr = lr
         self.discount = discount
         self.epsilon = epsilon
+
+        self.rewards = []
         
         # TODO: Implement
         self.start_epsilon_decay = 1
-        self.end_epsilon_decay = 9_000//2
+        self.end_epsilon_decay = EPISODES - EPISODES*0.1
         self.epsilon_decay_value = self.epsilon/(self.end_epsilon_decay - self.start_epsilon_decay)
 
         self.Q = self.initiate_Q(obs_space, action_space)
@@ -91,6 +99,8 @@ class QAgent:
         executed = False
         obs = observation
 
+        rew = 0
+
         for _ in range(num_actions):
             actions = [i[1] for i in environment.get_actions()]
 
@@ -106,6 +116,8 @@ class QAgent:
                     #print("BEST:", action)
                 
                 next_observation, reward, done, winner, executed = env.step(action)
+
+                rew += reward
                 
                 new_q_value = self.get_new_q_value(obs, next_observation, reward, action)
                 self.update_Q(obs, action, new_q_value)
@@ -127,16 +139,18 @@ class QAgent:
         
         self.epsilon -= self.epsilon_decay_value
         
-        return obs, done, winner
+        return obs, done, winner, rew
             
             
 
 
 env = gym.make('reduced_backgammon_gym:reducedBackgammonGym-v0')
 
+print(env.observation_space)
+
 # By printing the observation_space and action_space we see that
 # observation_space = [9, 9, 9, 9, 9, 9, 9, 2, 2, 3, 2] and action_space = [8, 8]
-observation_space = (9, 9, 9, 9, 9, 9, 9, 2, 2, 3, 2)
+observation_space = (9, 9, 9, 9, 9, 9, 9, 2, 2, 2, 2)
 action_space = (8, 8) # 0, ..., 7
 
 # SRC: 0, 1, 2, 3, 4, 5, 6, BAR
@@ -179,11 +193,14 @@ nr_winner = {WHITE: [], BLACK: []}
 
 last_state = None
 
-for _, i in tqdm(enumerate(range(10_000))):
+rews = []
+
+for _, i in tqdm(enumerate(range(EPISODES))):
 
     obs, current_agent = env.reset()
     winner, done = None, False
-    #env.render()
+
+    tot_rew = 0
 
     while not done:
 
@@ -194,8 +211,13 @@ for _, i in tqdm(enumerate(range(10_000))):
         #print(agent)
         #print(env.gym.non_used_dice)
 
+        if i == EPISODES - 1:
+            env.render()
+
+
         if env.current_agent == BLACK:
-            obs, done, winner = agent.apply_action(env, obs)
+            obs, done, winner, rew = agent.apply_action(env, obs)
+            tot_rew += rew
         else:
             obs, done, winner = agent.apply_random_action(env, obs)
 
@@ -206,18 +228,32 @@ for _, i in tqdm(enumerate(range(10_000))):
         
         env.change_player_turn()
 
-        #env.render()
 
-    if i % 20 == 0:
+        #env.render()
+    
+    rews.append(tot_rew)
+
+    if i % 100 == 0:
         print()
         print("BLACK:", sum(nr_winner[BLACK]) / (sum(nr_winner[BLACK]) + sum(nr_winner[WHITE])))
         print("WHITE:", sum(nr_winner[WHITE]) / (sum(nr_winner[BLACK]) + sum(nr_winner[WHITE])))
 
-print(nr_winner)
 print(agents[BLACK].Q[last_state])
 print(agents[BLACK].get_best_action_given_observation(last_state))
 print(agents[BLACK].Q[last_state + agents[BLACK].get_best_action_given_observation(last_state)])
 print(last_state)
 
-print(sum(nr_winner[BLACK][9_000:]))
-print(sum(nr_winner[WHITE][9_000:]))
+print(sum(nr_winner[BLACK][int(EPISODES * 0.9):]))
+print(sum(nr_winner[WHITE][int(EPISODES * 0.9):]))
+
+avgs = [sum(rews[x:x + STEPS])/STEPS for x in range(0, len(rews), STEPS)]
+wins = [sum(nr_winner[BLACK][x:x + STEPS])/STEPS for x in range(0, len(nr_winner[BLACK]), STEPS)]
+
+
+plt.plot(avgs)
+plt.title(f"AVG REWARDS | EP: {EPISODES}, lr:{agents[BLACK].lr}, eps: {agents[BLACK].epsilon}, stps: {STEPS}, disc: {agents[BLACK].discount}")
+plt.show()
+
+plt.plot(wins)
+plt.title(f"WINS | EP: {EPISODES}, lr:{agents[BLACK].lr}, eps: {agents[BLACK].epsilon}, stps: {STEPS}, disc: {agents[BLACK].discount}")
+plt.show()
